0d0adcab599e66cc42b944e00300bf87ad8d940e
schultzberg <viktor.schultzberg@gmail.com>
2016-02-23 14:25:49 +0100

    added archive-course and single-course

--- wp-content/themes/flat/index.php
+++ wp-content/themes/flat/index.php
@@ -3,21 +3,22 @@
 	<div id="content" class="site-content" role="main">
 		<?php flat_hook_index_top(); ?>
 
-		<?php $user = wp_get_current_user();
+		<?php 
+		$user = wp_get_current_user(); 
 		if ( in_array( 'subscriber', (array) $user->roles ) ) {
 		    get_template_part('student_landing');
 		}
 		else {
 		?>
-		<?php if ( have_posts() ) : ?> <!-- the loop -->
-		<?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'content', get_post_format() ); ?>
-		<?php endwhile; ?>
-		<?php the_posts_pagination( array( 'prev_text' => __( '<i class="fa fa-chevron-left"></i>', 'flat' ), 'next_text' => __( '<i class="fa fa-chevron-right"></i>', 'flat' ) ) ); ?>
-	<?php else : ?>
-		<?php get_template_part( 'content', 'none' ); ?>
-	<?php endif; ?>	<!-- ends loop -->
-<?php } ?> <!-- ends if/else -->
+			<?php if ( have_posts() ) : ?> <!-- the loop -->
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'content', get_post_format() ); ?>
+			<?php endwhile; ?>
+			<?php the_posts_pagination( array( 'prev_text' => __( '<i class="fa fa-chevron-left"></i>', 'flat' ), 'next_text' => __( '<i class="fa fa-chevron-right"></i>', 'flat' ) ) ); ?>
+		<?php else : ?>
+			<?php get_template_part( 'content', 'none' ); ?>
+		<?php endif; ?>	<!-- ends loop -->
+		<?php } ?> <!-- ends if/else -->
 		<?php flat_hook_index_bottom(); ?>
 	</div>
 	<?php flat_hook_index_after(); ?>
--- wp-content/themes/flat/student_landing.php
+++ wp-content/themes/flat/student_landing.php
@@ -1,15 +1,15 @@
-
-
 <?php
 $user_id = get_current_user_id();
 //print_r(get_user_meta($user_id));
 global $current_user;
 get_currentuserinfo();
 ?>
-
-<div class="row">
-	<div class="col-xs-9">
-		<h1>Welcome <?php echo $current_user->display_name ?></h1>
+<?php flat_hook_index_before(); ?>
+<div id="content" class="site-content" role="main">
+	<?php flat_hook_index_top(); ?>
+	<header class="entry-header">
+		<h1 class="entry-title" itemprop="name"> Welcome <?php echo $current_user->display_name ?></h1>
+	</header>
 
 	<?php
 		
@@ -28,35 +28,39 @@ get_currentuserinfo();
 		      echo get_the_author_meta('gplus', $user_id);
 */
 ?>
+</div>
+<div class="entry-content" itemprop="articleBody">
+	<h1>Submit student report</h1>
 
-<h1>Submit student report</h1>
+	<?php		
+		echo'	
+			<form method="post" name="front_end" action="" >
+				<input type="text" name="title" placeholder="Report Title..." /><br>
+				<textarea cols="75" rows="15" name="content" placeholder="Report Content..."></textarea>
+				<input type="submit">
+				<input type="hidden" name="action" value="report" />
+				<input type="hidden" name="author" value="'.$user_id.'" />
 
-<?php		
-	echo'	
-		<form method="post" name="front_end" action="" >
-			<input type="text" name="title" placeholder="Report Title..." /><br>
-			<textarea cols="75" rows="15" name="content" placeholder="Report Content..."></textarea>
-			<input type="submit">
-			<input type="hidden" name="action" value="report" />
-			<input type="hidden" name="author" value="'.$user_id.'" />
+			</form>';
+	?>
 
-		</form>';
-?>
+	<h1>Previous reports</h1>
+	<?php
+	$reports = new WP_query(['post_type' => 'report', 'author' => $user_id]);
 
-<h1>Previous reports</h1>
-<?php
-$reports = new WP_query(['post_type' => 'report', 'author' => $user_id]);
-
-if( $reports->have_posts() ) :
-		while($reports->have_posts() ) : $reports->the_post();
-		?>
-				<h4><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h4>
-				<p>Posted: <?php the_time("Y:m:d H:m") ?></p>
-					
-			<?php
-			endwhile;	
-else :
-	Echo 'No posts';
-endif;	
-?>
-	</div><!--col-->
+	if( $reports->have_posts() ) :
+			while($reports->have_posts() ) : $reports->the_post();
+			?>
+					<h4><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h4>
+					<p>Posted: <?php the_time("Y:m:d H:m") ?></p>
+						
+				<?php
+				endwhile;	
+	else :
+		Echo 'No posts';
+	endif;	
+	?>
+	<?php flat_hook_index_bottom(); ?>
+</div>
+	<?php flat_hook_index_after(); ?>
+<?php get_footer(); ?>
