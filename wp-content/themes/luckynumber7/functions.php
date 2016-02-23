2750179bfe580ef767db9ef9e50ba2dd4cf89492
schultzberg <viktor.schultzberg@gmail.com>
2016-02-23 09:53:07 +0100

    added P2P for examans, and created a course page

--- wp-content/themes/luckynumber7/functions.php
+++ wp-content/themes/luckynumber7/functions.php
@@ -1,4 +1,4 @@
-<?php
+    <?php
 ######################## Custom post types ################################
 function post_type_student_init() {
     //For dashboard view
@@ -91,9 +91,9 @@ function post_type_course_init() {
         'rewrite'            => array( 'slug' => 'course' ),
         'capability_type'    => 'post',
         'has_archive'        => true,
-        'hierarchical'       => false,
+        'hierarchical'       => true,
         'menu_position'      => null,
-        'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments'),
+        'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments', 'page-attributes'),
         //'register_meta_box_cb' => 'add_portfolio_metabox'
 
     );
@@ -103,6 +103,71 @@ function post_type_course_init() {
 }
 
 add_action('init', 'post_type_course_init');
+
+
+function post_type_course_components_init() {
+    //For dashboard view
+    $labels = array(
+            'name'                  => "Course Components",
+            'singular_name'         => "Course Component",
+            'menu_name'             => "Course Components",
+            'name_admin_bar'        => "Course Components",
+            'add_new'               => __( 'Add New', 'textdomain' ),
+            'add_new_item'          => __( 'Add New Component', 'textdomain' ),
+            'new_item'              => __( 'New Component', 'textdomain' ),
+            'edit_item'             => __( 'Edit Component', 'textdomain' ),
+            'view_item'             => __( 'View Component', 'textdomain' ),
+            'all_items'             => __( 'All Component', 'textdomain' ),
+            'search_items'          => __( 'Search Component', 'textdomain' ),
+            'parent_item_colon'     => __( 'Parent Component:', 'textdomain' ),
+            'not_found'             => __( 'No Component found.', 'textdomain' ),
+            'not_found_in_trash'    => __( 'No Component found in Trash.', 'textdomain' ),
+            'featured_image'        => _x( 'Component Cover Image', 'Overrides the “Featured Image” phrase for this post type. Added in 4.3', 'textdomain' ),
+            'set_featured_image'    => _x( 'Set cover image', 'Overrides the “Set featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+            'remove_featured_image' => _x( 'Remove cover image', 'Overrides the “Remove featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+            'use_featured_image'    => _x( 'Use as cover image', 'Overrides the “Use as featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+            'archives'              => _x( 'Component archives', 'The post type archive label used in nav menus. Default “Post Archives”. Added in 4.4', 'textdomain' ),
+            'insert_into_item'      => 'Insert into Component',//_x( 'Insert into Component', 'Overrides the “Insert into post”/”Insert into page” phrase (used when inserting media into a post). Added in 4.4', 'textdomain' ),
+            'uploaded_to_this_item' => _x( 'Uploaded to this Component', 'Overrides the “Uploaded to this post”/”Uploaded to this page” phrase (used when viewing media attached to a post). Added in 4.4', 'textdomain' ),
+            'filter_items_list'     => _x( 'Filter Courses list', 'Screen reader text for the filter links heading on the post type listing screen. Default “Filter posts list”/”Filter pages list”. Added in 4.4', 'textdomain' ),
+            'items_list_navigation' => _x( 'Courses list navigation', 'Screen reader text for the pagination heading on the post type listing screen. Default “Posts list navigation”/”Pages list navigation”. Added in 4.4', 'textdomain' ),
+            'items_list'            => _x( 'Courses list', 'Screen reader text for the items list heading on the post type listing screen. Default “Posts list”/”Pages list”. Added in 4.4', 'textdomain' ),
+        );
+    //Arguments for our new post type
+    $args = array(
+        'labels'             => $labels,
+        'public'             => true,
+        'publicly_queryable' => true,
+        'show_ui'            => true,
+        'show_in_menu'       => true,
+        'query_var'          => true,
+        'rewrite'            => array( 'slug' => 'component' ),
+        'capability_type'    => 'post',
+        'has_archive'        => true,
+        'hierarchical'       => true,
+        'menu_position'      => true,
+        'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments'),
+        //'register_meta_box_cb' => 'add_portfolio_metabox'
+
+    );
+
+    // Registers our custom post type with above arguments
+    register_post_type('component', $args);
+}
+
+add_action('init', 'post_type_course_components_init');
+
+
+function my_connection_types() {
+    p2p_register_connection_type( array( 
+        'name' => 'custom_post_manager',
+        'from' => 'course',
+        'to' => 'component',
+        'cardinality' => 'one-to-many',
+        'title' => array( 'from' => 'Managed by', 'to' => 'Manages' )
+    ) );
+}
+add_action( 'p2p_init', 'my_connection_types' );
 
 
 ######################## Taxonomies ################################
